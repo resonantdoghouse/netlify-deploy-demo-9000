e577cdf23fe65628d046fcaff1f99ef8cca38aa6
Jim <jim@advancebass.com>
2023-08-29 12:26:07 -0700

    test deploy :rocket:

--- src/pages/Inventory.js
+++ src/pages/Inventory.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import DonutSpinner from "../components/DonutSpinner";
 
+
 const API_URL = process.env.REACT_APP_API_URL;
 
 const Inventory = () => {
